499c9a4cae827498e130c8d07a41adc47fa1d4a3
Ashhar909 <94983829+Ashhar909@users.noreply.github.com>
2022-07-06 00:25:34 +0530

    Video2 done

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        Todays news is blah blah
+      </div>
+    )
+  }
+}
+
+export default NewsItem
