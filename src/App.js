41525d8c5a0fc38b5c520185ffb3e5fb2c413b74
Ashhar909 <94983829+Ashhar909@users.noreply.github.com>
2022-07-14 17:09:07 +0530

    commit after t5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
-import News from './components/News';
+// import News from './components/News';
 // import Demo from './components/Demo';
+import Demo2 from './components/Demo2'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <News />
+        {/* <News /> */}
         {/* <Demo /> */}
+        <Demo2 />
       </div>
     )
   }
